fd3cb01f9757df87399165e661f95b987d788a6d
Saradhi Indurthi <saradhix@yahoo.com>
2014-11-06 10:36:55 -0800

    Improvised etl

--- etl.php
+++ etl.php
@@ -2,11 +2,15 @@
 include "libcurl.php";
 include "rules.php";
 
-define("MAX_RULES",2);
+$rule_hits=array();
+$sentences=array();
+
+define("MAX_RULES",4);
 $query="flipkart";
 
 
 $page="http://en.wikipedia.org/wiki/Flipkart";
+//$page="http://en.wikipedia.org/wiki/Mahatma_Gandhi";
 
 $header = get_web_page($page);
 
@@ -20,11 +24,50 @@ if($http_code ==0)
 $content = $header['content'];
 process_page($content);
 printf("max_rules=%d\n",MAX_RULES);
+//Now process the rule_hits array
 
+foreach($rule_hits as $sentence_hits)
+{
+  $event=array();
+  foreach($sentence_hits as $hit)
+  {
+    print_r($hit);
+    $event['sid']=$hit['sid'];
+    if(isset($hit['day'])) $event['day']=$hit['day'];
+    if(isset($hit['month'])) $event['month']=$hit['month'];
+    if(isset($hit['year'])) $event['year']=$hit['year'];
+    $eid=sprintf("%4d%02d%02d",$event['year'],$event['month'],$event['day']);
+    $eid=ceil($eid);
+
+    echo "End of hit with index $eid\n";
+  }
+  $events[$eid]=$event;
+}
+ksort($events,SORT_NUMERIC);
+print_r($events);
+//printing in formatted way
+foreach($events as $event)
+{
+print_event($event);
+}
+$cx=count($events);
+echo "Count=$cx\n";
+function print_event($event)
+{
+global $sentences;
+global $months;
+$year=$event['year'];
+$month=$months[$event['month']];
+$day=$event['day'];
+$sentence=$sentences[$event['sid']];
+echo "$year-$month-$day $sentence\n";
+}
 
 
 function process_page($content)
 {
+  global $rule_hits;
+  global $sentences;
   $len=strlen($content);
   echo "Content size=$len\n";
   $docObj = new DOMDocument();
@@ -41,6 +84,23 @@ function process_page($content)
   echo "\n\n\nCount=$count\n";
   //do some preprocessing to  support .coms
   $content = str_replace(".[",". [",$content);
+  $content = preg_replace('/[\x00-\x1F\x80-\x9F]/u', ' . ', $content);
+  $end_pos= strrpos ( $content , 'References' );
+  $start_pos=strpos($content,'From Wikipedia');
+  if($start_pos === false)
+    $start_pos = 0;
+  if($end_pos === false)
+    $end_pos = strlen($content);
+
+  $content = substr($content,$start_pos, $end_pos - $start_pos);
+
+  //extract content between 'From Wikipedia, the free encyclopedia' till references section
+
+  //$content=get_content_between($content, 'From Wikipedia','References');
+  $content = strip_square_brackets($content);
+  $content = replace_square_brackets($content);
+  $content = replace_brackets($content);
+  $content = strip_duplicate_spaces($content);
   $sentences = explode('. ',$content);
   print_r($sentences);
   $count = count($sentences);
@@ -49,23 +109,33 @@ function process_page($content)
   for($i=0;$i<$count-1;$i++)
   {
     $sentence=$sentences[$i];
-    process_sentence($sentence);
+    process_sentence($sentence,$i);
   }
+  print_r($rule_hits);
+  printf("Number of hits=%d\n",count($rule_hits));
 }
 
-function process_sentence($sentence)
+function process_sentence($sentence,$sentence_id)
 {
   //$sentence = preg_replace('/[\x00-\x1F\x80-\xFF]/', '', $sentence);
-  $sentence = preg_replace('/[\x00-\x1F\x80-\x9F]/u', '', $sentence);
-  $sentence = strip_duplicate_spaces($sentence);
   $sentence = str_replace('\r', '', $sentence);
   $sentence = str_replace('\n', '', $sentence);
+  $sentence = str_replace('\t', '', $sentence);
   $sentence = trim($sentence);
   $sentence = strtolower($sentence);
+  if(strlen($sentence) < 20) return;
+  if(strlen($sentence) > 150) return;
+  //display_bytes($sentence);
+  //return;
   for($i=0;$i<MAX_RULES;$i++)
   {
     $fun="rule_$i";
-    $fun($sentence);
+    $ret = $fun($sentence,$sentence_id);
+    if($ret)
+    {
+      print_r($ret);
+      //var_dump($ret);
+    }
   }
 
 }
@@ -80,4 +150,43 @@ function strip_duplicate_spaces($content)
   }
   while($content !=$new);
   return $content;
+}
+
+function strip_square_brackets($string)
+{
+  $string=preg_replace('/\[\d*\]/',"",$string);
+  return $string;
+}
+
+function replace_square_brackets($string)
+{
+  $string=str_replace("[","",$string);
+  $string=str_replace("]","",$string);
+  return $string;
+}
+
+function replace_brackets($string)
+{
+  $string=str_replace("(","",$string);
+  $string=str_replace(")","",$string);
+  return $string;
+}
+function display_bytes($var)
+{
+  echo "Var=$var\n\n\n\n\n";
+  for($i = 0; $i < strlen($var); $i++)
+  {
+    echo "c=".$var[$i]."code=".ord($var[$i])."\n";
+  }
+}
+
+
+function get_content_between($content, $start, $end)
+{
+  $r = explode($start, $content);
+  if (isset($r[count($r)-1])){
+    $r = explode($end, $r[count($r)-1]);
+    return $r[0];
+  }
+  return '';
 }
--- rules.php
+++ rules.php
@@ -1,24 +1,134 @@
 <?php
-function rule_0($sentence)
+$months=array("January","February","March","April","May","June","July","August",
+              "September","October","November","December");
+function rule_0($sentence,$sentence_id)
 {
-//check for presence of structure like
-//in 2007
+  global $rule_hits;
 
-//we will split the sentence on in, and then for each element from pos 1, check
-//if there is a 4 digits and a space.
-$arr = explode("in ",$sentence);
-$count = count($arr);
-for($i=1;$i<$count;$i++)
-{
-  if(is_year($arr[$i]))
+  //check for presence of structure like
+  //in 2007
+
+  //we will split the sentence on in, and then for each element from pos 1, check
+  //if there is a 4 digits and a space.
+  $arr = explode(" in ",$sentence);
+  $count = count($arr);
+  for($i=1;$i<$count;$i++)
   {
-    printf("rule_0::match found for %s in sentence $sentence\n",$arr[$i]);
+    if($year=is_year($arr[$i]))
+    {
+      printf("rule_0::match found for %s in sentence %s on fragment=%d\n",
+             $arr[$i],$sentence,$i);
+      $hit=array();
+      $hit["sid"]=$sentence_id;
+      $hit["rule"]=0;
+      $hit["year"]=$year;
+      $hit["sentence"]=$arr[$i-1]." in ".$arr[$i];
+      $rule_hits[$sentence_id][]=$hit;
+    }
   }
 }
+
+function rule_1($sentence,$sentence_id)
+{
+  global $rule_hits;
+  if($year=has_year($sentence))
+  {
+    $hit=array();
+    $hit["sid"]=$sentence_id;
+    $hit["rule"]=1;
+    $hit["year"]=$year;
+    $hit["sentence"]=$sentence;
+    $rule_hits[$sentence_id][]=$hit;
+    return;
+  }
+}
+function rule_2($sentence,$sentence_id)
+{
+  global $rule_hits;
+  global $months;
+  $max_months=count($months);
+  $present_months=array();
+
+  //search if there are any months and if yes, note them aside
+
+  for($i=0;$i<$max_months;$i++)
+  {
+    //printf("ISMONTH::%s\n",$months[$i]);
+    if(strstr($sentence,strtolower($months[$i]))||strstr($sentence,
+    substr(strtolower($months[$i]),0,3)))
+    {
+      $present_months[]=$i;
+    }
+  }
+  if(!$present_months) return;
+  for($year=2014;$year>1000;$year--)
+  {
+    for($i=0;$i<count($present_months);$i++)
+    {
+      $str="$year ".strtolower($months[$present_months[$i]]);
+      if(strstr($sentence,$str))
+      {
+        $hit=array();
+        $hit["sid"]=$sentence_id;
+        $hit["rule"]=2;
+        $hit["year"]=$year;
+        $hit["month"]=$month;
+        $hit["sentence"]=$sentence;
+        $rule_hits[$sentence_id][]=$hit;
+      }
+      $str=strtolower($months[$present_months[$i]])." $year";
+      if(strstr($sentence,$str))
+      {
+        $hit=array();
+        $hit["sid"]=$sentence_id;
+        $hit["rule"]=2.1;
+        $hit["year"]=$year;
+        $hit["month"]=$present_months[$i];
+        $hit["sentence"]=$sentence;
+        $rule_hits[$sentence_id][]=$hit;
+      }
+    }
+  }
 }
 
-function rule_1($sentence)
+function rule_3($sentence, $sentence_id)
 {
+  global $rule_hits;
+  global $months;
+  $present_months=array();
+  $max_months=count($months);
+  for($i=0;$i<$max_months;$i++)
+  {
+    for($day=31;$day>0;$day--)
+    {
+      $str=substr(strtolower($months[$i]),0,3)." ".$day." ";
+      //echo "rule3 checking $str\n";
+      if(strstr($sentence,$str))
+      {
+        $hit=array();
+        $hit["sid"]=$sentence_id;
+        $hit["rule"]=3;
+        $hit["month"]=$i;
+        $hit["day"]=$day;
+        $hit["sentence"]=$sentence;
+        $hit["search"]=$str;
+        $rule_hits[$sentence_id][]=$hit;
+      }
+      $str=$day." ".substr(strtolower($months[$i]),0,3);
+      //echo "rule3 checking $str\n";
+      if(strstr($sentence,$str))
+      {
+        $hit=array();
+        $hit["sid"]=$sentence_id;
+        $hit["rule"]=3;
+        $hit["month"]=$i;
+        $hit["day"]=$day;
+        $hit["sentence"]=$sentence;
+        $hit["search"]=$str;
+        $rule_hits[$sentence_id][]=$hit;
+      }
+    }
+  }
 }
 
 function is_year($part)
@@ -27,8 +137,32 @@ function is_year($part)
   for($i=2014;$i>1000;$i--)
   {
     if($i==$year)
-      return 1;
+      return $year;
   }
   return 0;
 }
-
+function has_year($part)
+{
+  //echo "entered has_year with $part\n";
+  for($i=2014;$i>1000;$i--)
+  {
+    if(strstr($part,"$i"))
+    {
+      //echo "Match found for $i\n";
+      return $i;
+    }
+  }
+  return 0;
+}
+function last_few_tokens($string,$howmany)
+{
+  $ret='';
+  $arr=explode(" ",$string);
+  $count=count($arr);
+  $count-=$howmany;
+  for($i=0;$i<=$howmany;$i++)
+  {
+    $ret.= $arr[$count+$i]." ";
+  }
+  return trim($ret);
+}
